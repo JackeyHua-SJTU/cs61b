10847299a8eaff2fc2b20bb3c316363541b9829b
Jackey Hua <jackeyhua@sjtu.edu.cn>
2022-08-11 22:00:27 +0800

    change the 2nd func again which went wrong at the second try.

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,6 +82,10 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
+        if(A == null){
+            A = B;
+            return A;
+        }
         IntList tmp = A;
         while(tmp.rest != null){
             tmp = tmp.rest;
