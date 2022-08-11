ef0f16f8dadd8ccfab2f66a2200b640f292910de
Jackey Hua <jackeyhua@sjtu.edu.cn>
2022-08-11 22:45:09 +0800

    add disc dir

---
+++ disc/README.txt
@@ -0,0 +1,1 @@
+This is the directory for all possible codes for discussion paper even though there has been answers
---
+++ disc/week2.java
@@ -0,0 +1,3 @@
+public class week2{
+    public int String = "Why uploading a file into GitHub successfully is so tricky!!!!";
+}
