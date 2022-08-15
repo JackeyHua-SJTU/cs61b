70f9264b446b497e848f5530b08597c8081dbb68
Jackey Hua <jackeyhua@sjtu.edu.cn>
2022-08-15 14:51:03 +0800

    7th edition

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,79 +1,116 @@
-public class ArrayDeque<item> {
+import org.junit.Test;
 
+public class ArrayDeque<T> {
+    private T[] array;
+    private int size;
+    private int len;
     private int nextFirst;
     private int nextLast;
-    private item[]items;
-    private int size;
-    public ArrayDeque(){
-        items=(item[])new Object[8];
-        nextFirst=items.length-1;
-        nextLast=0;
-        size=0;
-    }
-    private void resize(int capacity){
-        item[]a=(item[])new Object[capacity];
-        System.arraycopy(items,0,a,0,nextLast);
-        int length=items.length-1-nextFirst;
-        System.arraycopy(items,nextFirst+1,a,capacity-length,length);
-        nextFirst=capacity-length-1;
-        items=a;
-    }
-    public void addFirst(item item) {
-        if (nextFirst-1==nextLast)
-            resize(items.length*2);
-        items[nextFirst]=item;
-        size++;
-        nextFirst--;
-    }
 
-    public void addLast(item item) {
-        if (nextFirst-1==nextLast)
-            resize(items.length*2);
-        items[nextLast]=item;
-        size++;
-        nextLast++;
+    /** front points to the index before the start
+     *  nextFirst points to the place next first element will be put in
+     *  nextLast refers to the place next last element will be put in
+     */
+    public ArrayDeque() {
+        len = 0;
+        array = (T[]) new Object[8];
+        size = 8;
+        nextFirst = 0;
+        nextLast = 1;
+    }
+    @Test
+    public void addFirst(T item) {
+        if (len == size - 1) {
+            resize();
+        }
+        array[nextFirst] = item;
+        len += 1;
+        nextFirst = (nextFirst - 1 + size) % size;
+    }
+    @Test
+    public void addLast(T item) {
+        if (len == size - 1) {
+            resize();
+        }
+        array[nextLast] = item;
+        len += 1;
+        nextLast = (nextLast + 1) % size;
     }
 
     public boolean isEmpty() {
-        return size==0;
+        return (len == 0);
     }
 
     public int size() {
-        return size;
+        return len;
     }
 
     public void printDeque() {
-        for (int i=nextFirst+1;i!=nextLast-1;i=(i+1)%items.length)
-            System.out.print(items[i]+" ");
-        System.out.print(items[nextLast-1]);
+        if (len == 0) {
+            return;
+        }
+        int iterateTime = (nextLast - nextFirst - 1 + size) % size;
+        for (int i = 1; i < iterateTime; i += 1) {
+            System.out.print((nextFirst + i) % size);
+            System.out.print(' ');
+        }
+        System.out.print((nextFirst + iterateTime) % size);
     }
-
-    public item removeFirst() {
-        if (nextFirst==items.length-1)return null;
-        nextFirst++;
-        item temp=items[nextFirst];
-        items[nextFirst]=null;
-        size--;
-        if (items.length>=16&&size<items.length/4)
-            resize(items.length/2);
-        return temp;
-    }
-
-    public item removeLast() {
-        if (nextLast==0)return null;
-        nextLast--;
-        item temp=items[nextLast];
-        items[nextLast]=null;
-        size--;
-        if (items.length>=16&&size<items.length/4)
-            resize(items.length/2);
-        return temp;
-    }
-
-    public item get(int index) {
-        if (index>=size)
+    @Test
+    public T removeFirst() {
+        if (isEmpty()) {
             return null;
-        return items[(nextFirst+1+index)%items.length];
+        }
+        nextFirst = (nextFirst + 1) % size;
+        T num = array[nextFirst];
+        array[nextFirst] = null;
+        len -= 1;
+        resize();
+        return num;
+    }
+    @Test
+    public T removeLast() {
+        if (isEmpty()) {
+            return null;
+        }
+        nextLast = (nextLast - 1 + size) % size;
+        T num = array[nextLast];
+        array[nextLast] = null;
+        len -= 1;
+        resize();
+        return num;
+    }
+    @Test
+    public T get(int index) {
+        if (index < 0 || index >= len) {
+            return null;
+        } else {
+            return array[(nextFirst + 1 + index) % size];
+        }
+    }
+
+    private void resize() {
+        if (len == size - 1) {
+            T[] newArray = (T[]) new Object[size * 2];
+            for (int i = 0; i < len; i += 1) {
+                newArray[i] = array[(nextFirst + i + 1) % size];
+            }
+            array = newArray;
+            size = size * 2;
+            nextFirst = size - 1;
+            nextLast = len;
+        } else if (size >= 16 && len / size <= 0.25) {
+            T[] newArray = (T[]) new Object[size / 2];
+            for (int i = 0; i < len; i += 1) {
+                newArray[i] = array[(nextFirst + i + 1) % size];
+            }
+            array = newArray;
+            size = size / 2;
+            nextFirst = size - 1;
+            nextLast = len;
+        } else {
+            return;
+        }
     }
 
 }
