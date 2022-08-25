9071cd3f9bc361a7ef9b1ac432c979b531ca8164
Jackey Hua <jackeyhua@sjtu.edu.cn>
2022-08-25 21:28:39 +0800

    1st edition hope to past gradescope

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,105 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private int size;
+    private WeightedQuickUnionUF uf;
+    private boolean[] open;
+    private int openSites;
+    private int top;
+    private int bottom;
+
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new java.lang.IllegalArgumentException();
+        }
+        this.size = N;
+        this.uf = new WeightedQuickUnionUF(N * N + 2);
+        this.open = new boolean[N * N];
+        this.top = N * N;
+        this.bottom = N * N + 1;
+        for (int i = 0; i < N * N; i += 1) {
+            open[i] = false;
+        }
+        for (int i = 0; i < N; i += 1) {
+            uf.union(top, i);
+        }
+        for (int i = N * (N - 1); i < N * N; i += 1){
+            uf.union(bottom, i);
+        }
+        this.openSites = 0;
+    }
+    private int xyTo1d(int row, int col) {
+        if (row < 0 || row >= size || col < 0 || col >= size) {
+            return -1;
+        }
+        return size * row + col;
+    }
+
+    public void open(int row, int col) {
+        if (row < 0 || row >= size || col < 0 || col >= size) {
+            throw new java.lang.IndexOutOfBoundsException();
+        }
+        if (isOpen(row, col)) {
+            return;
+        }
+
+        int xy = xyTo1d(row, col);
+        open[xy] = true;
+        openSites += 1;
+        int[] temp = new int[4];
+        temp[0] = xyTo1d(row - 1, col);
+        temp[1] = xyTo1d(row, col - 1);
+        temp[2] = xyTo1d(row, col + 1);
+        temp[3] = xyTo1d(row + 1, col);
+        for (int i : temp) {
+            if (i <= 0) {
+                continue;
+            }
+            if (open[i] && (uf.connected(i, top) || isFull(row, col))) {
+                uf.union(i, xy);
+            }
+            if (open[i] && open[xy]) {
+                uf.union(i, xy);
+            }
+        }
+    }
+
+    public boolean isOpen(int row, int col) {
+        if (row < 0 || row >= size || col < 0 || col >= size) {
+            throw new java.lang.IndexOutOfBoundsException();
+        }
+        return open[xyTo1d(row, col)];
+    }
+
+    public boolean isFull(int row, int col) {
+        if (row < 0 || row >= size || col < 0 || col >= size) {
+            throw new java.lang.IndexOutOfBoundsException();
+        }
+        return uf.connected(xyTo1d(row, col), top) && isOpen(row, col);
+    }
+
+    public int numberOfOpenSites() {
+        return openSites;
+    }
+
+    public boolean percolates() {
+        return uf.connected(top, bottom);
+    }
+
+    public static void main(String[] args) {
+        Percolation a = new Percolation(3);
+        a.open(0, 0);
+        System.out.println(a.isFull(0,0));
+        a.open(1, 2);
+        System.out.println(a.isFull(1,0));
+        a.open(1, 1);
+        System.out.println(a.isFull(1,1));
+        a.open(2, 1);
+        System.out.println(a.isFull(2,1));
+        a.open(0, 2);
+        System.out.println(a.uf.connected(0, 9));
+        System.out.println(a.numberOfOpenSites());
+        System.out.print(a.percolates());
+    }
 
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,47 @@
 package hw2;
 
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
 public class PercolationStats {
+    private double[] ans;
+    private Percolation pc;
+    private double mean;
+    private double stddev;
+    private int time;
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0) {
+            throw new java.lang.IllegalArgumentException();
+        }
+        time = T;
+        ans = new double[T];
+        int index = 0;
+        for (int i = 0; i < T; i += 1) {
+            pc = pf.make(N);
+            while (!pc.percolates()) {
+                int rand1 = StdRandom.uniform(0, N);
+                int rand2 = StdRandom.uniform(0, N);
+                pc.open(rand1, rand2);
+            }
+            ans[index] = (double)pc.numberOfOpenSites() / N;
+        }
+        mean = StdStats.mean(ans);
+        stddev = StdStats.stddev(ans);
+    }
+
+    public double mean() {
+        return mean;
+    }
+
+    public double stddev() {
+        return stddev;
+    }
+
+    public double confidenceLow() {
+        return mean - 1.96 * Math.sqrt(stddev / time);
+    }
+
+    public double confifenceHigh() {
+        return mean + 1.96 * Math.sqrt(stddev / time);
+    }
 
 }
