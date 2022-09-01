362bce88e54b4a9f8f8c6c292bee722d8dac09a9
Jackey Hua <jackeyhua@sjtu.edu.cn>
2022-09-01 11:39:22 +0800

    finish lab11 2nd

--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -1,5 +1,7 @@
 package lab11.graphs;
 
+import edu.princeton.cs.algs4.Stack;
+
 /**
  *  @author Josh Hug
  */
@@ -9,16 +11,55 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
-
+    private Maze maze;
+    private int s;
+    private int[] alterEdge;
     public MazeCycles(Maze m) {
         super(m);
+        maze = m;
+        s = 0;
+        alterEdge = new int[maze.V()];
+        alterEdge[0] = 0;
     }
 
     @Override
     public void solve() {
         // TODO: Your code here!
+        Stack<Integer> tmp = new Stack<>();
+        tmp.push(s);
+        marked[s] = true;
+        announce();
+
+        while (!tmp.isEmpty()) {
+            int temp = tmp.pop();
+
+            for (int x : maze.adj(temp)) {
+                if (!marked[x]) {
+                    marked[x] = true;
+                    alterEdge[x] = temp;
+                    continue;
+                }
+
+                if (marked[x] == true && x != alterEdge[temp]) {
+                    alterEdge[alterEdge[x]] = x;
+                    alterEdge[x] = temp;
+                    change(x);
+                    announce();
+                    return;
+                }
+            }
+        }
     }
 
     // Helper methods go here
+    private void change(int x) {
+        int tmp = x;
+        while (alterEdge[tmp] != x) {
+            edgeTo[tmp] = alterEdge[tmp];
+            tmp = alterEdge[tmp];
+        }
+        edgeTo[tmp] = x;
+        announce();
+    }
 }
 
